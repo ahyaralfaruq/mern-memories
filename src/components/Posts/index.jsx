9a3e4fb4797d127f6e4682f545d23afb88a89abe
ahyaralfaruq <ahyaralfaruq97@gmail.com>
2022-07-26 02:06:02 +0700

    memubat project memories menggunakan axios, moment, react-redux, redux, redux thunk, react-file-base64, material-ui/icon (--force), material-ui/core (--force) pada react versi 18.2.0 dan set up penggunaannya.

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,30 @@
+import { CircularProgress, Grid } from "@material-ui/core";
+import React from "react";
+import { useSelector } from "react-redux";
+import Post from "./Post";
+
+import useStyles from "./styles";
+
+const Index = ({ setCurrentId }) => {
+   const classes = useStyles();
+   const posts = useSelector((state) => state.posts);
+
+   return !posts.length ? (
+      <CircularProgress />
+   ) : (
+      <Grid
+         className={classes.container}
+         container
+         alignItems="stretch"
+         spacing={3}
+      >
+         {posts.map((post) => (
+            <Grid item key={post._id} xs={12} sm={6}>
+               <Post post={post} setCurrentId={setCurrentId} />
+            </Grid>
+         ))}
+      </Grid>
+   );
+};
+
+export default Index;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Form from "./Form";
+import Posts from "./Posts";
+
+export { Form, Posts };
